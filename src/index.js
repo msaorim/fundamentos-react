b4417399beba311ebcab1c1d5180ee8b76fa8f7f
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-20 16:24:37 -0300

    conhecendo jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import React from 'react'
 import ReactDom from 'react-dom'
 
 const el = document.getElementById('root')
+const linha = <h2>Olá gente!!!</h2>
 
-ReactDom.render("Olá gente!!!", el)
+ReactDom.render(<div>{linha}</div>, el)
