d075d178291497aef3d293a12a284a25c13ab95d
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-20 18:54:29 -0300

    primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
+import Primeiro from './components/basicos/Primeiro'
 
 const el = document.getElementById('root')
 const linha = <h2>Olá gente!!!</h2>
 
-ReactDom.render(<div>{linha}</div>, el)
+ReactDom.render(
+<div>
+    <Primeiro></Primeiro>
+</div>, 
+el)
