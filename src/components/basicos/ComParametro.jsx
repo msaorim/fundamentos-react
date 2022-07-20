dc3099e873a18853deca209aef843503b591567b
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-07-20 19:53:55 -0300

    componente com propriedades

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+export default function ComParametro(props){
+    const status = props.nota >= 7 ? "Aprovado" : "Reprovado"
+    return(
+        <div>
+            <h2>{props.titulo}</h2>
+            <p>{props.aluno} tem a nota: {props.nota}</p>
+            <p>Resultado: {status}</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 const el = document.getElementById('root')
 const linha = <h2>Olá gente!!!</h2>
 
 ReactDom.render(
 <div>
-    <Primeiro></Primeiro>
+    <Primeiro />
+    <ComParametro 
+        titulo = "Situação do Aluno"
+        aluno = "Pedro"
+        nota = {9.3} />
+    <ComParametro 
+        titulo = "Situação do Aluno"
+        aluno = "Maria"
+        nota = {6.3} />
 </div>, 
 el)
