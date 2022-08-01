09b794b5248f5f2ebd5221b07be608b19b927503
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-08-01 13:46:59 -0300

    Card 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
+import './App.css';
 import React from "react";
 
 import Primeiro from './components/basicos/Primeiro'
@@ -8,35 +9,34 @@ import Aleatorio from "./components/Aleatorio";
 import Card from "./components/layout/Card";
 
 export default () => (
-    <div id='app'>
+    <div className='app'>
         <h1>Fundamentos React</h1>
-        <Card title="Desafio Aleatório">
-        <Aleatorio min={1} max={100}/>
-        <Aleatorio min={1} max={100}/>
-        <Aleatorio min={1} max={100}/>
-        </Card>
+        <div className="cards">
+            <Card title="#04 - Desafio Aleatório">
+                <Aleatorio min={1} max={100} />
+            </Card>
 
-        <Card title="Fragmento">
-        <Fragmento />
-        </Card>
+            <Card title="#03 - Fragmento">
+                <Fragmento />
+            </Card>
 
-        <Card title="Com Parâmetro 1">
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro"
-            nota={9.3} />
-        </Card>
+            <Card title="#02 - Com Parâmetro 1">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro"
+                    nota={9.3} />
+            </Card>
 
-        <Card title="Com Parâmetro 2">
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Maria"
-            nota={6.3} />
-        </Card>
+            <Card title="#02 - Com Parâmetro 2">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Maria"
+                    nota={6.3} />
+            </Card>
 
-        <Card title="Primeiro">
-        <Primeiro />
-        </Card>
-
+            <Card title="#01 - Primeiro">
+                <Primeiro />
+            </Card>
+        </div>
     </div>
 );
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -4,7 +4,7 @@ import "./Card.css";
 export default function Card(props) {
     return (
         <div className="card">
-            <div className="title">Título: {props.title}</div>
+            <div className="title">{props.title}</div>
             <div className="content">{props.children}</div>
         </div>
     )
